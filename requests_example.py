f20bd480e1fa855b87d406129e29381fb1fb7957
Chmele <killerbbb123@gmail.com>
2024-09-21 00:12:27 +0300

    move and extend examples

--- requests_example.py
+++
@@ -1,7 +0,0 @@
-import requests
-from key import KEY
-from qradar import QRadar
-
-
-q = QRadar("https://qradar.local", KEY, "22.0", requests.Session(), verify=False)
-print(q.get_reference_data_sets("refset"))
