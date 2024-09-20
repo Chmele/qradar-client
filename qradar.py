42b39e7b4d12eb13e22b4d57475c75b87421f23c
Chmele <killerbbb123@gmail.com>
2024-09-20 16:17:30 +0300

    initial

---
+++ qradar.py
@@ -0,0 +1,27 @@
+class QRadar:
+    def __init__(self, url, key, version, transport, verify=True):
+        self.url, self.key, self.session = f"{url}/api", key, transport
+        self.session.headers.update(
+            {"Accept": "application/json", "Version": version, "SEC": key}
+        )
+        self.session.verify = verify
+        self.api_methods = {
+            f"{(method:=endpoint.get("http_method").lower())}{(path:=endpoint.get("path"))
+            .replace("/", "_")}": self.api_endpoint_factory(
+                method, path
+            )
+            for endpoint in self.api_endpoint_factory("GET", "/help/endpoints")(
+                filter=f"version={version}"
+            )
+        }
+
+    def __getattr__(self, name: str):
+        return self.api_methods.get(name) or super().__getattribute__(name)
+
+    def api_endpoint_factory(self, method, url):
+        return lambda path=None, json=None, **params: self.session.request(
+            method,
+            f"{self.url}/{url}{f'/{path}' if path else ''}",
+            params=params,
+            json=json,
+        ).json()
