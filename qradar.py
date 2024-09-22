d78909c409e957be97a13465acb30282072f5d44
Chmele <killerbbb123@gmail.com>
2024-09-23 00:09:08 +0300

    fixes and removal of redundant args

--- qradar.py
+++ qradar.py
@@ -1,27 +1,24 @@
 class QRadar:
     def __init__(self, url, key, version, transport, verify=True):
-        self.url, self.key, self.session = f"{url}/api", key, transport
+        self.url, self.session = f"{url}/api", transport
         self.session.headers.update(
             {"Accept": "application/json", "Version": version, "SEC": key}
         )
         self.session.verify = verify
-        self.api_methods = {
+        self.__dict__.update({
             f"{(method:=endpoint.get("http_method").lower())}{(path:=endpoint.get("path"))
             .translate({ord('{'):None, ord('}'): None, ord('/'): ord('_')})}": self.api_endpoint_factory(
-                method, path
+                method.upper(), path
             )
             for endpoint in self.api_endpoint_factory("GET", "/help/endpoints")(
                 filter=f"version={version}"
             )
-        }
-
-    def __getattr__(self, name: str):
-        return self.api_methods.get(name) or super().__getattribute__(name)
+        })
 
     def api_endpoint_factory(self, method, url):
-        return lambda path=None, json=None, **params: self.session.request(
+        return lambda json=None, **params: self.session.request(
             method,
-            f"{self.url}/{url}{f'/{path}' if path else ''}".format(**params),
+            f"{self.url}{url}".format(**params),
             params=params,
             json=json,
         ).json()
