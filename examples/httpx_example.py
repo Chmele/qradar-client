cc1f4b5345549f4d9fd36fcb34647e6c11e12f54
Chmele <killerbbb123@gmail.com>
2024-11-06 14:45:06 +0200

    update examples

--- examples/httpx_example.py
+++ examples/httpx_example.py
@@ -4,32 +4,29 @@ from key import KEY
 
 class QRadar:
     def __init__(self, url, key, version, transport, verify=True):
-        self.url, self.key, self.session = f"{url}/api", key, transport
+        self.url, self.session = f"{url}/api", transport
         self.session.headers.update(
             {"Accept": "application/json", "Version": version, "SEC": key}
         )
         self.session.verify = verify
-        self.api_methods = {
+        self.__dict__.update({
             f"{(method:=endpoint.get("http_method").lower())}{(path:=endpoint.get("path"))
-            .replace("/", "_")}": self.api_endpoint_factory(
-                method, path
+            .translate({ord('{'):None, ord('}'): None, ord('/'): ord('_')})}": self.api_endpoint_factory(
+                method.upper(), path
             )
             for endpoint in self.api_endpoint_factory("GET", "/help/endpoints")(
                 filter=f"version={version}"
             )
-        }
-
-    def __getattr__(self, name: str):
-        return self.api_methods.get(name) or super().__getattribute__(name)
+        })
 
     def api_endpoint_factory(self, method, url):
-        return lambda path=None, json=None, **params: self.session.request(
+        return lambda json=None, **params: self.session.request(
             method,
-            f"{self.url}/{url}{f'/{path}' if path else ''}",
+            f"{self.url}{url}".format(**params),
             params=params,
             json=json,
         ).json()
 
 
-q = QRadar("https://qradar.local", KEY, "22.0", httpx.Client(verify=False))
-print(q.get_reference_data_sets("refset"))
+q = QRadar("https://qradar.is.local", KEY, "22.0", httpx.Client(verify=False))
+print(q.get_ariel_searches())
