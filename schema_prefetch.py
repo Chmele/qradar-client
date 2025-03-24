5f4a75bad7754db95c9133d101fc81555f92e619
Vsevolod Hermaniuk <53478956+Chmele@users.noreply.github.com>
2025-03-24 20:28:00 +0200

    fix stub incorrect syntax

--- schema_prefetch.py
+++ schema_prefetch.py
@@ -1,6 +1,6 @@
+import inspect
 import ast
 from qradar import QRadar
-
 
 
 def unparse(ast_obj):
@@ -9,74 +9,70 @@ def unparse(ast_obj):
 
 
 class Stubgen:
-    def __init__(self, class_name, version, scheme):
-        class_def = ast.ClassDef(
-            name=class_name,
-            bases=[],
-            keywords=[],
-            body=[],
-            decorator_list=[]
+    def __init__(self, cls, scheme):
+        module = ast.parse(inspect.getsource(cls))
+        class_def = module.body[0]
+        module.body.insert(
+            0,
+            ast.ImportFrom(
+                "typing",
+                [
+                    ast.alias(name="Optional"),
+                    ast.alias(name="Dict"),
+                    ast.alias(name="Any"),
+                ],
+            ),
         )
         for endpoint in scheme:
             class_def.body.append(self.prepare_method(endpoint))
         with open("qradar.pyi", "w") as file:
-            file.write(unparse(class_def))
-
+            file.write(unparse(module))
 
     def prepare_method(self, scheme):
-        http_method = scheme.get("http_method")
-        path = scheme.get("path")
-        method_name = f"{http_method.lower()}{path.replace('/', '_')}"
-
-        args = [
-            ast.arg(arg='self', annotation=None),
-            ast.arg(arg='path', annotation=ast.Subscript(
-                value=ast.Name(id='Optional', ctx=ast.Load()),
-                slice=ast.Index(value=ast.Name(id='str', ctx=ast.Load())),
-                ctx=ast.Load()
-            )),
-            ast.arg(arg='json', annotation=ast.Subscript(
-                value=ast.Name(id='Optional', ctx=ast.Load()),
-                slice=ast.Index(value=ast.Name(id='Dict', ctx=ast.Load())),
-                ctx=ast.Load()
-            )),
-        ]
-        
-        func_args = ast.arguments(
-            posonlyargs=[],
-            args=args,
-            vararg=ast.arg(arg='params', annotation=None),
-            kwonlyargs=[],
-            kw_defaults=[],
-            defaults=[],
-            kwarg=None
-        )
-        docstring = f"""{scheme.get('description')}"""
-
-        doc_expr = ast.Expr(
-            value=ast.Constant(value=docstring)
-        )
-        
-        body = [
-            doc_expr,
-            ast.Expr(value=ast.Constant(value=...))
-        ]
-        
-        returns = ast.Name(id='Any', ctx=ast.Load())
-        
-        func_def = ast.FunctionDef(
-            name=method_name,
-            args=func_args,
-            body=body,
+        return ast.FunctionDef(
+            name=f"""{
+                scheme.get("http_method").lower()}{scheme.get("path")
+                .translate({ord('{'):None, ord('}'): None, ord('/'): ord('_')})}""",
+            args=ast.arguments(
+                posonlyargs=[],
+                args=[
+                    ast.arg(arg="self", annotation=None),
+                    ast.arg(
+                        arg="path",
+                        annotation=ast.Subscript(
+                            value=ast.Name(id="Optional", ctx=ast.Load()),
+                            slice=ast.Index(value=ast.Name(id="str", ctx=ast.Load())),
+                            ctx=ast.Load(),
+                        ),
+                    ),
+                    ast.arg(
+                        arg="json",
+                        annotation=ast.Subscript(
+                            value=ast.Name(id="Optional", ctx=ast.Load()),
+                            slice=ast.Index(value=ast.Name(id="Dict", ctx=ast.Load())),
+                            ctx=ast.Load(),
+                        ),
+                    ),
+                ],
+                vararg=ast.arg(arg="params", annotation=None),
+                kwonlyargs=[],
+                kw_defaults=[],
+                defaults=[],
+                kwarg=None,
+            ),
+            body=[
+                ast.Expr(value=ast.Constant(value=f"""{scheme.get('description')}""")),
+                ast.Expr(value=ast.Constant(value=...)),
+            ],
             decorator_list=[],
-            returns=returns
+            returns=ast.Name(id="Any", ctx=ast.Load()),
         )
-        return func_def
-    
+
+
 from key import KEY
 import requests
 
-q = QRadar("https://10.1.12.222", KEY, "22.0", requests.Session(), False)
+q = QRadar("https://qradar.is.local", KEY, "22.0", requests.Session(), False)
 
 scheme = q.get_help_endpoints()
-s = Stubgen("QRadar", '22.0', scheme)
+s = Stubgen(QRadar, scheme)
