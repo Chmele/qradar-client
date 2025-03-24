f7509f0f491ce3796b64b9f54316c6b10820294b
Vsevolod Hermaniuk <53478956+Chmele@users.noreply.github.com>
2025-03-24 20:28:00 +0200

    provide cached client

--- qradar/__init__.py
+++ qradar/__init__.py
@@ -1,7 +1,9 @@
 from .async_client import QRadar as QRadarAsync
 from .client import QRadar
+from .cached_client import QRadarCached
 
 __all__ = [
     'QRadar',
-    'QRadarAsync'
+    'QRadarAsync',
+    'QRadarCached'
 ]
