040cae2f935470b677a1a3f903f4058040777d35
Vsevolod Hermaniuk <53478956+Chmele@users.noreply.github.com>
2025-03-24 20:28:00 +0200

    move and extend examples

--- httpx_example.py
+++
@@ -1,7 +0,0 @@
-import httpx
-from key import KEY
-from qradar import QRadar
-
-
-q = QRadar("https://qradar.local", KEY, "22.0", httpx.Client(verify=False))
-print(q.get_reference_data_sets("refset"))
